ad0a65a95ed386fb789adaba7dcde1a2d0dd46f3
Sawickyyy <igoriki0407@gmail.com>
2026-08-07 18:37:10 +0100

    Fix update repository configuration

--- DropLAN/UpdateSettings.cs
+++ DropLAN/UpdateSettings.cs
@@ -2,13 +2,8 @@ namespace DropLAN;
 
 public static class UpdateSettings
 {
-    // ZMIEŃ po utworzeniu repozytorium GitHub, np.
-    // https://github.com/Sawickyyy/DropLAN
     public const string GitHubRepositoryUrl =
         "https://github.com/Sawickyyy/DropLAN";
 
-    public static bool IsConfigured =>
-        !GitHubRepositoryUrl.Contains(
-            "Sawickyyy",
-            StringComparison.OrdinalIgnoreCase);
+    public const bool IsConfigured = true;
 }
